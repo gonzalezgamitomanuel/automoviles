153326f175889510fe1bc3a0d24c584eda0b651d
gonzalezgamitomanuel <72204428+gonzalezgamitomanuel@users.noreply.github.com>
2020-11-03 09:46:41 +0100

    Update automovil.ts

--- src/clases/automovil.ts
+++ src/clases/automovil.ts
@@ -1,119 +1,80 @@
 export class Automovil {
+    
+    private _id: string
+    private _gasto: number
+    private _arrancado: boolean
+    private _velocidad: number
 
-    static push: any
-    static forEach(a: (Automovil: { Id: any }) => void) {
-
-        throw new Error('error')
-
+    public constructor (id: string, gasto:number){
+        
+        this._id= id
+        this._gasto= gasto
+        this._arrancado= false
+        this._velocidad= 0
+        
     }
 
-    private id : any
-    private gasto : number
-    private gasol: number 
-    private encend : boolean 
-    private velocid : number
-
-    constructor (id:any, gasto:number, gasol:number){
-
-        this.id = id
-        this.gasto = gasto
-        this.gasol = gasol
-        this.encend = false 
-        this.velocid = 0
-
+    public get id(){
+        
+        return this._id
+        
     }
 
-    get Id(){
-
-        return this.id
-
+    public get gasto(){
+        
+        return this._gasto
+        
     }
 
-    get Gasto(){
-
-        return this.gasto
-
+    public get arrancado(){
+        
+        return this._arrancado
+        
     }
 
-    get Encend(){
-
-        return this.encend
-
+    public get velocidad(){
+        
+        return this._velocidad
+        
     }
 
-    get Gasol(){
-
-        return this.gasol
-
-    }
-
-    set Gasol(c:number){
-
-        if(this.gasol <= 0){
-
-            throw 'El deposito no tiene gasolina'
-
-        }
-        this.gasol=c
-
-    }
-
-    cArrancado(){
-
-        if(this.Encend==false){
-
-            this.encend=true
-
+    public estaArrancado(){
+        
+        if(this._arrancado==false){
+            
+            this._arrancado=true
+            
         }else{
-
-            if (this.velocid!=0){
-
-                throw 'La velocidad del automovil debe ser 0 para desactivarlo'
-
-            } else {
-
-                this.encend=false
-
-            }
+            
+            this._arrancado=false
+            this._velocidad=0
+            
         }
     }
 
-    get Velocid(){
-
-        return this.velocid
-
-    }
-
-    set Velocid(num:number){
-
-        if(this.encendido==false){
-
-            throw 'Para cambiar la velocidad el automovil debe estar en funcionamiento'
-
+    public set velocidad(v:number){
+        
+        if(this._arrancado==false){
+            
+            throw 'Error, si el coche está parado no se puede cambiar la velocidad'
+            
         } else {
-
-            this.velocid=num
-
+            
+            this._velocidad=v
+            
         }
     }
-   
-    gastado(t:number){ 
 
-        if(!this.encend || this.velocid==0){
-
-            throw 'No puede calcular el consumo del automovil estando desactivado'
-
-        } else {
-
-            return (this.velocid/t)*(this.gasto/100)
-
-        }
+    public totalGastado(t:number){
+        
+        return (this._velocidad/t)*(this._gasto/100)
+        
     }
-   
-    imprimir(){
 
-        return `El automovil con identificador ${this.id} va a ${this.velocid} km/h y consume ${this.gasto} litros de gasolina cada 100 km`
-
+    public mostrarCoche(){
+        
+        return `El coche con identificador ${this.id} va a una velocidad de ${this._velocidad} km/h y consume ${this._gasto} L/100 km`
     }
+
 
 }
